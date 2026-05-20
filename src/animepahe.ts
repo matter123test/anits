6b026f43699e53cb28e442d5987d7307caed100d
matter123test <84776941+matter123test@users.noreply.github.com>
2026-05-20 18:46:45 +0200

    Prints more stuff

---
+++ src/animepahe.ts
@@ -0,0 +1,149 @@
+import { type Browser, type BrowserContext, chromium } from 'playwright';
+import * as cheerio from "cheerio";
+
+interface AnimeSearchResponse {
+    total: number;
+    per_page: number;
+    current_page: number;
+    last_page: number;
+    next_page_url?: string;
+    prev_page_url?: string;
+    from: number;
+    to: number;
+    data: Array<{
+        id: number;
+        title: string;
+        type: string;
+        episodes: number;
+        status: string;
+        season: string;
+        year: number;
+        score: number;
+        poster: string;
+        session: string;
+    }>;
+}
+
+interface EpisodesFetchResponse {
+    total: number;
+    per_page: number;
+    current_page: number;
+    last_page: number;
+    next_page_url?: string;
+    prev_page_url?: string;
+    from: number;
+    to: number;
+    data: Array<{
+        id: number,
+        anime_id: number,
+        episode: number,
+        episode2: number,
+        edition: string,
+        title: string,
+        snapshot: string,
+        disc: string,
+        audio: string,
+        duration: string,
+        session: string,
+        filler: number,
+        created_at: string
+    }>;
+}
+
+interface EmbededSource {
+    text?: string | undefined ;
+    src?: string | undefined;
+    fansub?: string | undefined;
+    resolution?: string | undefined;
+    audio?: string | undefined;
+}
+
+export class Animepahe {
+    browser!: Browser;
+    context!: BrowserContext;
+
+    current_page = 1;
+
+    async init() {
+        this.browser = await chromium.launch({ headless: true });
+        this.context = await this.browser.newContext();
+
+        // Preload the website
+        const page = await this.context.newPage();
+        await page.goto("https://animepahe.pw");
+        await page.waitForLoadState('domcontentloaded');
+        await page.waitForSelector('.main');
+        await page.close();
+    }
+
+    async close() {
+        await this.context.close();
+        await this.browser.close();
+    }
+
+    async getAnime(query: string): Promise<AnimeSearchResponse> {
+        const url = `https://animepahe.pw/api?m=search&q=${encodeURIComponent(query)}`;
+        const res = await this.context.request.get(url);
+
+        if (!res.ok) {
+            throw new Error(`Failed to search anime, ${res.status}`);
+        }
+
+        let data;
+        try {
+            data = await res.json();
+        }
+        catch (err) {
+            console.log(await res.text());
+            throw new Error(`Failed to parse json, ${err}`);
+        }
+
+        return data;
+    }
+
+    async getEpisodes(animeSession: string): Promise<EpisodesFetchResponse> {
+        const url = `https://animepahe.pw/api?m=release&id=${animeSession}&sort=episode_desc&page=${this.current_page}`;
+        const res = await this.context.request.get(url);
+
+        if (!res.ok) {
+            throw new Error(`Failed to fetch episodes, with response: ${res.status()}`);
+        }
+
+        const data = await res.json();
+
+        return data;
+    }
+
+    nextPage() {
+        this.current_page++;
+    }
+
+    backPage() {
+        this.current_page--;
+    }
+
+    async getEmbededSources(animeSession: string, episodeSession: string): Promise<Array<EmbededSource>> {
+        const url = `https://animepahe.pw/play/${animeSession}/${episodeSession}`;
+
+        const page = await this.context.newPage();
+        await page.goto(url);
+        await page.waitForSelector('.dropup');
+
+        const $ = cheerio.load(await page.content());
+
+        const items = $('.dropdown-item')
+            .map((i, el) => ({
+                text: $(el).text().trim(),
+                src: $(el).attr('data-src')?.toString(),
+                fansub: $(el).attr('data-fansub'),
+                resolution: $(el).attr('data-resolution'),
+                audio: $(el).attr('data-audio'),
+            })).filter(
+                (i, el) => !!el.src
+            ).get();
+
+        await page.close();
+
+        return items;
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,159 +1,7 @@
 import { select, spinner, text } from '@clack/prompts';
-import { type Browser, type BrowserContext, chromium } from 'playwright';
-import * as cheerio from "cheerio";
 import { exec } from 'node:child_process';
+import { Animepahe } from './animepahe.js';
 
-interface AnimeSearchResponse {
-    total: number;
-    per_page: number;
-    current_page: number;
-    last_page: number;
-    next_page_url?: string;
-    prev_page_url?: string;
-    from: number;
-    to: number;
-    data: Array<{
-        id: number;
-        title: string;
-        type: string;
-        episodes: number;
-        status: string;
-        season: string;
-        year: number;
-        score: number;
-        poster: string;
-        session: string;
-    }>;
-}
-
-interface EpisodesFetchResponse {
-    total: number;
-    per_page: number;
-    current_page: number;
-    last_page: number;
-    next_page_url?: string;
-    prev_page_url?: string;
-    from: number;
-    to: number;
-    data: Array<{
-        id: number,
-        anime_id: number,
-        episode: number,
-        episode2: number,
-        edition: string,
-        title: string,
-        snapshot: string,
-        disc: string,
-        audio: string,
-        duration: string,
-        session: string,
-        filler: number,
-        created_at: string
-    }>;
-}
-
-interface EmbededSource {
-    text?: string;
-    src?: string;
-    fansub?: string;
-    resolution?: string;
-    audio?: string;
-}
-
-class Animepahe {
-    browser!: Browser;
-    context!: BrowserContext;
-
-    current_page = 1;
-
-    async init() {
-        this.browser = await chromium.launch({ headless: false });
-        this.context = await this.browser.newContext();
-
-        // Preload the website
-        const page = await this.context.newPage();
-        await page.goto("https://animepahe.pw");
-        await page.waitForLoadState('domcontentloaded');
-        await page.waitForSelector('.main');
-        await page.close();
-    }
-
-    async close() {
-        await this.context.close();
-        await this.browser.close();
-    }
-
-    throwFailedToFetchAnime(msg: string) {
-        throw new Error(`Failed to search anime, ${msg}`);
-    }
-
-    async getAnime(query: string): Promise<AnimeSearchResponse> {
-        const url = `https://animepahe.pw/api?m=search&q=${encodeURIComponent(query)}`;
-        const res = await this.context.request.get(url);
-
-        if (!res.ok) {
-            this.throwFailedToFetchAnime(res.status.toString());
-        }
-
-        let data;
-        try {
-            data = await res.json();
-        }
-        catch (err) {
-            console.log(await res.text());
-            throw new Error(`Failed to parse json, ${err}`);
-        }
-
-        return data;
-    }
-
-    async getEpisodes(animeSession: string): Promise<EpisodesFetchResponse> {
-        const url = `https://animepahe.pw/api?m=release&id=${animeSession}&sort=episode_desc&page=${this.current_page}`;
-        const res = await this.context.request.get(url);
-
-        if (!res.ok) {
-            throw new Error(`Failed to fetch episodes, with response: ${res.status()}`);
-        }
-
-        const data = await res.json();
-
-        return data;
-    }
-
-    nextPage() {
-        this.current_page++;
-    }
-
-    backPage() {
-        this.current_page--;
-    }
-
-    async getEmbededSources(animeSession: string, episodeSession: string): Promise<Array<EmbededSource>> {
-        const url = `https://animepahe.pw/play/${animeSession}/${episodeSession}`;
-
-        const page = await this.context.newPage();
-        await page.goto(url);
-        await page.waitForSelector('.dropup');
-
-        const $ = cheerio.load(await page.content());
-
-        const items = $('.dropdown-item')
-            .map((i, el) => ({
-                text: $(el).text().trim(),
-                src: $(el).attr('data-src'),
-                fansub: $(el).attr('data-fansub'),
-                resolution: $(el).attr('data-resolution'),
-                audio: $(el).attr('data-audio'),
-            })).filter(
-                (i, el) => !!el.src
-            )
-            .get();
-
-        await page.close();
-
-        return items;
-    }
-}
 
 function openUrl(url: string) {
     const cmd = process.platform === "win32"
@@ -178,21 +26,26 @@ async function main() {
     // let other = await api.getEpisodes(session!);
     // console.log(other);
 
+    // Search anime
     const animeSearch = await text({
         message: "Search anime: "
     });
 
+    s.start("Searching anime...");
     const animeResults = await api.getAnime(animeSearch.toString());
+    s.stop("Done searching anime");
 
     const animeSession = await select<string>({
         message: "Select anime",
         options: animeResults.data.map((anime) => ({ value: anime.session, label: anime.title }))
     });
 
+    // Select episode
     let episodeSession;
-
     while (true) {
+        s.start("Fetching episodes...");
         const episodeResults = await api.getEpisodes(animeSession.toString());
+        s.stop("Fetched episodes");
 
         let episodesOptions = episodeResults.data.map(
             (ep) => ({ value: ep.session, label: ep.episode.toString() })
@@ -221,7 +74,9 @@ async function main() {
         }
     }
 
+    s.start("Fetching url...");
     const sources = await api.getEmbededSources(animeSession.toString(), episodeSession.toString());
+    s.stop("Fetched urls");
 
     const source = await select<string>({
         message: "Select source",
